535f947e4c84823566c79b31322907370ead9007
bhavyabihani <yashikabihani7@gmail.com>
2021-05-17 16:03:55 +0530

    User and Admin Authentication

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -5,29 +5,37 @@ const mongoose = require("mongoose");
 const app = express();
 
 const PORT = 3000;
+
+//routes
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin/auth');
+
 env.config();
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.epnb3.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex : true
 }).then(() => {
     console.log("Database Connected");
 });
 
 // app.use(express.json());
 app.use(bodyParser());
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
 
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello from Server",
-  });
-});
+// app.get("/", (req, res, next) => {
+//   res.status(200).json({
+//     message: "Hello from Server",
+//   });
+// });
 
-app.post("/data", (req, res, next) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+// app.post("/data", (req, res, next) => {
+//   res.status(200).json({
+//     message: req.body,
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/src/routes/admin/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { signup , signin, requireSignin} = require('../../controller/admin/auth');
+const router = express.Router();
+
+router.post('/admin/signup', signup);
+router.post('/admin/signin', signin);
+
+// router.post('/profile', requireSignin, (req, res) => {
+//     res.status(200).json({user: 'profile'})
+// });
+
+module.exports = router;
